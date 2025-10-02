b0da78b390b0d0657e19ee57182e62599470ab0b
Benjamin <benjaminaravena999@gmail.com>
2025-10-02 17:12:41 -0300

    Agregado menu pricipal informacion

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
+from iu.menu_principal import menu_principal
+
 def app():
     verdadero = True
     while verdadero:
+        print(f"\nBienvenido a")
+        menu_principal()
+        print()
         opcion = input("Ingrese opción[0-4]: ")
         if opcion == '0':
             print("saliendo del sistema...")
