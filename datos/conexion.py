2be84402231c147895e4d8d5a64992e7b43b5f62
Benjamin <benjaminaravena999@gmail.com>
2025-10-25 21:35:36 -0300

    explicación de la instancia global a Session

--- datos/conexion.py
+++ datos/conexion.py
@@ -6,7 +6,7 @@ try:
     cadena_conexion = "mysql+mysqlconnector://root:@localhost:3306/red_social"
     motor_db = create_engine(cadena_conexion)
     Session = sessionmaker(bind=motor_db)
-    sesion = Session()
+    sesion = Session() # Creamos la instancia de Sessión aquí, para que esta sea usada de manera global por todos los negocios y así evitamos problemas al momento de aplicar crud en la base de datos, ya que si creamos varias instancias de Session, cada una tendrá su propio caché y generará problemas al momento de aplicar crud en los datos (cambios no visibles hasta refrescar)
 
 except Exception as e:
     print(f"Error {e}")
