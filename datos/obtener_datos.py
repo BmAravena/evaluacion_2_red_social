2ec50eb68e80fb15f1fe39b635c312ab321a1a75
Benjamin <benjaminaravena999@gmail.com>
2025-11-12 17:02:46 -0300

    Cuarta parte del comentado del código

--- datos/obtener_datos.py
+++ datos/obtener_datos.py
@@ -1,6 +1,6 @@
 from datos.conexion import sesion
 
-
+# Función global para obtener datos de cualquier clase, se devuelve una lista con todos los datos para posteriormente ser utilizados
 def obtener_datos(clase):
     datos = sesion.query(clase).all()
     if datos:    
